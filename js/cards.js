3f38dd11bf341dcc5c5af155db4146e449489a91
Matias Gabriel Cabrera <matias_g.c@hotmail.com>
2022-08-05 18:02:52 -0300

    :sparkle: Feat: added the fucntion to show characters to the cards page

--- js/cards.js
+++ js/cards.js
@@ -6,13 +6,53 @@ const fetchCharacters = async (url) => {
 
         if (response.ok) {
             const characters = await response.json();
-            return characters;
-        } else {
-            return {};
+
+            return { status: response.status, characters};
+        }
+        else {
+            return { status: response.status };
         }
     }
     catch (error) {
-        console.error(error);
         return { error };
     }
 }
+
+const addCharacterToPage = (character, cardsContainer) => {
+    const characterCard = document.createElement("div");
+    characterCard.classList = "card";
+
+    characterCard.innerHTML = `
+        <img class="card--img" src="${character.image}" alt="${character.name}" loading="lazy">
+        <h3 class="card--character-name">${character.name}</h3>
+        <p class="card--character-info">Status: ${character.status}</p>
+        <p class="card--character-info">Species: ${character.species}</p>
+        <p class="card--character-info">Gender: ${character.gender}</p>
+        <p class="card--character-info">Origin: ${character.origin.name}</p>
+        <p class="card--character-info">Location: ${character.location.name}</p>
+    `;
+
+    cardsContainer.appendChild(characterCard);
+}
+
+const renderCharacters = async (url) => {
+    const data = await fetchCharacters(url);
+
+    if (data.error) {
+        console.error(data.error);
+    }
+    else {
+        const cardsContainer = document.querySelector("#cards-container");
+
+        if (data.status !== 200 ) {
+            console.warn(`There is a problem with the request. Status code: ${data.status}`);
+        }
+        else {
+            const listOfCharacters = data.characters.results;
+
+            listOfCharacters.forEach(character => addCharacterToPage(character, cardsContainer));
+        }
+    }
+}
+
+renderCharacters(URL_GET_CHARACTERS);
