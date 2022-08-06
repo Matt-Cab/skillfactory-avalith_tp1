2511e50bb8e22bd9dc1a539adfbe48bd7d8cc8c4
Matias Gabriel Cabrera <matias_g.c@hotmail.com>
2022-08-06 19:57:23 -0300

    :sparkle: Feat: added functions to fetch and add users to the table in users page

--- js/users.js
+++ js/users.js
@@ -1,1 +1,51 @@
 const URL_GET_USERS = "https://jsonplaceholder.typicode.com/users";
+
+const fetchUsers = async () => {
+    try {
+        const response = await fetch(URL_GET_USERS);
+
+        if (response.ok) {
+            const users = await response.json();
+            return { status: response.status, users };
+        }
+
+        return { status: response.status }
+    }
+    catch (error) {
+        return { error };
+    }
+}
+
+const addUserToTable = (user, table) => {
+    const userRow = document.createElement("tr");
+
+    userRow.innerHTML = `
+        <td>${user.id}</td>
+        <td>${user.name}</td>
+        <td>${user.name}</td>
+        <td>${user.phone}</td>
+        <td>${user.address.city}</td>
+        <td>${user.company.name}</td>
+    `;
+
+    table.appendChild(userRow);
+}
+
+const renderUsers = async () =>{
+    const data = await fetchUsers();
+
+    if (data.error) {
+        console.error(data.error);
+    }
+    else {
+        const tableOfUsers = document.querySelector(".table-users-container");
+
+        if (data.status === 200) {
+            const listOfUsers = data.users;
+
+            listOfUsers.forEach(user => addUserToTable(user, tableOfUsers));
+        }
+    }
+}
+
+renderUsers();
